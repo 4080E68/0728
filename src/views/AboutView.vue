b096a4826fe4f19fc76cab4bc23cb30946bd728b
Toad <zxcv96197@gmail.com>
2022-07-28 15:54:26 +0800

    0728

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,17 @@
+<!-- eslint-disable vue/no-unused-components -->
+<!-- eslint-disable quotes -->
 <template>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
 </template>
+<Card></Card>
+<script>
+import Card from "@/components/Card.vue";
+export default {
+  components: {
+    Card,
+  },
+};
+// eslint-disable-next-line eol-last
+</script>
